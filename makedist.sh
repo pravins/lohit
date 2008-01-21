a18dd9e8c4e6b8e68f7f968bd0a3d25543f07c32
Rahul Bhalerao <b.rahul.pm@gmail.com>
2008-01-21 10:32:35 +0000

    Merging back the changes in branch to the head
    git-svn-id: file:///srv/svn/lohit/trunk@49 193e51fd-57a6-47c4-bf68-4cb77cfaebef

--- makedist.sh
+++ makedist.sh
@@ -3,9 +3,9 @@
 echo Creating tarball of Checked Out Lohit Font files distribution
 sh generate.sh
 ver=`cat ChangeLog | grep "Current Version :-" | awk '{ print $4}'`
-mv TTFfiles fonts-indic-$ver
-cp COPYING ChangeLog README AUTHORS fonts-indic-$ver
-cd fonts-indic-$ver
+mv TTFfiles lohit-fonts-$ver
+cp COPYING ChangeLog README AUTHORS lohit-fonts-$ver
+cd lohit-fonts-$ver
 mkdir -p bengali gujarati  hindi  kannada  malayalam  oriya  punjabi  tamil  telugu
 mv lohit_bn.ttf bengali
 mv lohit_gu.ttf gujarati
@@ -17,6 +17,6 @@ mv lohit_pa.ttf punjabi
 mv lohit_ta.ttf tamil 
 mv lohit_te.ttf telugu
 cd ..
-tar -cf fonts-indic-$ver.tar fonts-indic-$ver
-gzip fonts-indic-$ver.tar
-rm -rf fonts-indic-$ver
+tar -cf lohit-fonts-$ver.tar lohit-fonts-$ver
+gzip lohit-fonts-$ver.tar
+rm -rf lohit-fonts-$ver
