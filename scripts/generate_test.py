0b6382602bc1d7d43a59c1faa04c8daf5ec02a9f
pravins <psatpute@redhat.com>
2014-01-30 14:07:45 +0530

    updated auto test scripts

--- scripts/generate_test.py
+++ scripts/generate_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -i*-
 # Copyright (C) 2013-14, Sneha Kore <skore@redhat.com>, Pravin Satpute <psatpute@redhat.com>
-# This script requires hb-shape utility from available in harfbuzz-devel rpm
+# This script requires hb-shape utility from available in harfbuzz-devel rpm 
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,37 +16,34 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os,sys,commands
+import os, sys, commands
 
-def auto_test(txt_file,ttf_file):
+def generate_stdfile(txt_file,ttf_file):
 	inputfile=open(txt_file)
-	outputfile=open("failed_test_case.txt","w")
+	outputfile=open("std-test-out.txt","w")
 
 	#Read the test-case input	
 	flines=inputfile.readlines()
-	count=0
-
-	#Exceute hb-shape command for each test-case from output file
+	
+	#Exceute hb-shape command for each test-case from input file
 	for string in flines:
+		#print "String : "+string
 		words=string.split()	
 		status, output = commands.getstatusoutput("hb-shape %s %s"%(ttf_file,words[0]))
-		# Test to check, wheather test-case from output file & the result, are matching		
-		if words[1] != output:
-			print words[0]+ " [FAILURE]\n"	
-			outputfile.write("  *  "+words[0]+"\t"+""+output+"\n")
-			count=count+1
-
-	#Count for failed test-cases	
-	print "%d Test Cases Failed out of %d"%(count,len(flines))
-	print "failed_test_case.txt file generated !!"
-	inputfile.close()
+		# Write output to the output file		
+		# print "Output : "	+output+"\n"	
+		outputfile.write(words[0]+"\t"+""+output+"\n")
+	print "std-test-out.txt file generated!!"
 	outputfile.close()
+	inputfile.close()
+
+
 
 if __name__ == "__main__":
 
-	if len(sys.argv) < 3:
-	 	print  " USAGE: python test.py <test file> <font_file> "
-	else:
-		txt_file = sys.argv[1]
-		font_file = sys.argv[2]
-		auto_test(txt_file,font_file)
+ if len(sys.argv) < 3:
+        print  " USAGE: python generate_test.py <test file> <font_file> "
+ else:
+	txt_file = sys.argv[1]
+	font_file = sys.argv[2]
+	generate_stdfile(txt_file,font_file)
