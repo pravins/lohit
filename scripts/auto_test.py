34fa953d780e9baf1c96d047b3911114c210f8ae
Pravin Satpute <psatpute@redhat.com>
2015-01-28 15:52:48 +0530

    added auto-test file

--- scripts/auto_test.py
+++ scripts/auto_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding: UTF-8 -i*-
 # Copyright (C) 2013-14, Sneha Kore <skore@redhat.com>, Pravin Satpute <psatpute@redhat.com>
 # This script requires hb-shape utility from available in harfbuzz-devel rpm
